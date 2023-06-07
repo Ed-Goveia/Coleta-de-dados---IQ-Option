88d9ccab651f445c7ed29bf9bbbd7b4de2428529
Eduardo Miguel <103007758+Ed-Goveia@users.noreply.github.com>
2023-06-07 21:06:49 +0000

    Clear

--- Test-Y/teste.py
+++
@@ -1,7 +0,0 @@
-import time
-from iqoptionapi.stable_api import IQ_Option
-Iq=IQ_Option("ed.mi.goveia@gmail.com","24030907*Ed*")
-Iq.connect()#connect to iqoption
-goal="EURUSD"
-print("get candles")
-print(Iq.get_candles(goal,60,111,time.time()))
